0e1c479122f8bd2696c080cc60a7ae6f8aca3cc5
Ayush <ayushjain2608@gmail.com>
2021-05-19 15:22:55 +0530

    Data cleaned csv file created

--- data_cleaning.py
+++ data_cleaning.py
@@ -50,12 +50,52 @@ Company name : Text Only
 df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating'] < 0 else x['Company Name'][:-3],axis = 1)
 # df = df.drop('company_txt',axis = 1)
 
+"""
+State Field
+"""
+
+df['job_state']  = df['Location'].apply(lambda x: x.split(',')[1])
+# df['job_state'].value_counts() # to see the how many jobs are there in states
+# to check if jobs are in headquarters
+df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis = 1)
+
+"""
+Age of company
+"""
+
+df['age'] = df['Founded'].apply(lambda x: x if x <1 else 2021 - x)
+
+"""
+Parsing Job Description(Python, Excel, Etc.)
+"""
 
 
+#python
+df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+df.python_yn.value_counts()
+#r studio 
+df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+df.R_yn.value_counts()
+
+#spark 
+df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+df.spark.value_counts()
+
+#aws 
+df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+df.aws.value_counts()
+
+#excel
+df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+df.excel.value_counts()
 
 
+# droping unnamed:0 column
+df_final = df.drop(['Unnamed: 0'], axis =1)
 
 
+#creating new cleaned CSV
+df_final.to_csv('salary_data_cleaned.csv',index = False)
 
 
 
