4e2b9a856e1e351672d2d67d2e683dd01470cc3c
Ayush <ayushjain2608@gmail.com>
2021-05-28 19:28:01 +0530

    Model Building .py File added

---
+++ data_modeling.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 12:55:42 2021
+
+@author: ayushjain9
+
+
+# Choose Relevant columns
+# Get dummy variable . Every categorical col should have each col
+# train test split
+# multiple linear regression
+# lasso regression
+# random forest
+# tune model usiing GridSearchCV
+# test ensembles
+
+
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('eda_data.csv')
+
+# Choose Relevant columns
